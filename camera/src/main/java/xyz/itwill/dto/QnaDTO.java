a01784934b386450ce3043c351d55b5a52328a1c
bela2525 <yg01270@naver.com>
2024-07-29 12:19:57 +0900

    qna

--- camera/src/main/java/xyz/itwill/dto/QnaDTO.java
+++ camera/src/main/java/xyz/itwill/dto/QnaDTO.java
@@ -1,109 +1,112 @@
 package xyz.itwill.dto;
 
-/*
- CREATE TABLE QNA (
-    QNA_NO NUMBER CONSTRAINT QNA_NO_PK PRIMARY KEY,
-    QNA_TITLE VARCHAR2(1000),
-    QNA_CONTENT VARCHAR2(2000),
-    QNA_STATUS NUMBER(1),
-    QNA_DATE DATE DEFAULT SYSDATE,
-    QNA_PROD_NO NUMBER CONSTRAINT QNA_PROD_NO_FK REFERENCES PRODUCT(PROD_NO) 
-    QNA_USERS_NO NUMBER CONSTRAINT QNA_USERS_NO_FK REFERENCES USERS(USERS_NO),
-    QNA_ANSWER VARCHAR2(2000),
-);
+/*CREATE TABLE QNA (
+QNA_NO NUMBER CONSTRAINT QNA_NO_PK PRIMARY KEY,
+QNA_USERS_NO NUMBER CONSTRAINT QNA_USERS_NO_FK REFERENCES USERS(USERS_NO),  
+QNA_TYPE VARCHAR2(300),
+QNA_TITLE VARCHAR2(1000),
+QNA_CONTENT VARCHAR2(2000),
+QNA_STATUS NUMBER(1),
+QNA_DATE DATE DEFAULT SYSDATE);
+*/
 
-CREATE SEQUENCE QNA_SEQ;
-
-DESC QNA;
-
-SELECT * FROM QNA;
-
- */
 public class QnaDTO {
-	private int qnaNo;
-	private String qnaTitle;
-	private String qnaContent;
-	private int qnaStatus;//0:삭제글,1:일반글,2:비밀글	
-	private String qnaDate;
-	private int qnaProdNo;
-	private int qnaUsersNo;//로그인 사용자의 회원번호
-	private String usersName;//users 테이블의 회원이름 저장하기 위한 필드 - 작성자
-	private String qnaAnswer;
-	
-	public QnaDTO() {
-		// TODO Auto-generated constructor stub
-	}
+    private int qnaNo;
+    private int qnaUsersNo;	//로그인 사용자의 회원번호
+    private String usersName; //users 테이블의 회원이름 저장하기 위한 필드 - 작성자
+    private String usersEmail;
+    private String qnaType; //문의 유형
+    private String qnaTitle;
+    private String qnaContent;
+    private int qnaStatus;	//1: 미답변, 2: 답변완료 
+    private String qnaDate;
+    
+    public QnaDTO() {
+    }
 
-	public int getQnaNo() {
-		return qnaNo;
-	}
+    public QnaDTO(int qnaNo, int qnaUsersNo, String usersName, String usersEmail, String qnaType,
+                  String qnaTitle, String qnaContent, int qnaStatus, String qnaDate) {
+        super();
+        this.qnaNo = qnaNo;
+        this.qnaUsersNo = qnaUsersNo;
+        this.usersName = usersName;
+        this.usersEmail = usersEmail;
+        this.qnaType = qnaType;
+        this.qnaTitle = qnaTitle;
+        this.qnaContent = qnaContent;
+        this.qnaStatus = qnaStatus;
+        this.qnaDate = qnaDate;
+    }
 
-	public void setQnaNo(int qnaNo) {
-		this.qnaNo = qnaNo;
-	}
+    public int getQnaNo() {
+        return qnaNo;
+    }
 
-	public String getQnaTitle() {
-		return qnaTitle;
-	}
+    public void setQnaNo(int qnaNo) {
+        this.qnaNo = qnaNo;
+    }
 
-	public void setQnaTitle(String qnaTitle) {
-		this.qnaTitle = qnaTitle;
-	}
+    public int getQnaUsersNo() {
+        return qnaUsersNo;
+    }
 
-	public String getQnaContent() {
-		return qnaContent;
-	}
+    public void setQnaUsersNo(int qnaUsersNo) {
+        this.qnaUsersNo = qnaUsersNo;
+    }
 
-	public void setQnaContent(String qnaContent) {
-		this.qnaContent = qnaContent;
-	}
+    public String getUsersName() {
+        return usersName;
+    }
 
-	public int getQnaStatus() {
-		return qnaStatus;
-	}
+    public void setUsersName(String usersName) {
+        this.usersName = usersName;
+    }
 
-	public void setQnaStatus(int qnaStatus) {
-		this.qnaStatus = qnaStatus;
-	}
+    public String getUsersEmail() {
+        return usersEmail;
+    }
 
-	public String getQnaDate() {
-		return qnaDate;
-	}
+    public void setUsersEmail(String usersEmail) {
+        this.usersEmail = usersEmail;
+    }
 
-	public void setQnaDate(String qnaDate) {
-		this.qnaDate = qnaDate;
-	}
+    public String getQnaType() {
+        return qnaType;
+    }
 
-	public int getQnaProdNo() {
-		return qnaProdNo;
-	}
+    public void setQnaType(String qnaType) {
+        this.qnaType = qnaType;
+    }
 
-	public void setQnaProdNo(int qnaProdNo) {
-		this.qnaProdNo = qnaProdNo;
-	}
+    public String getQnaTitle() {
+        return qnaTitle;
+    }
 
-	public int getQnaUsersNo() {
-		return qnaUsersNo;
-	}
+    public void setQnaTitle(String qnaTitle) {
+        this.qnaTitle = qnaTitle;
+    }
 
-	public void setQnaUsersNo(int qnaUsersNo) {
-		this.qnaUsersNo = qnaUsersNo;
-	}
+    public String getQnaContent() {
+        return qnaContent;
+    }
 
-	public String getUsersName() {
-		return usersName;
-	}
+    public void setQnaContent(String qnaContent) {
+        this.qnaContent = qnaContent;
+    }
 
-	public void setUsersName(String usersName) {
-		this.usersName = usersName;
-	}
+    public int getQnaStatus() {
+        return qnaStatus;
+    }
 
-	public String getQnaAnswer() {
-		return qnaAnswer;
-	}
+    public void setQnaStatus(int qnaStatus) {
+        this.qnaStatus = qnaStatus;
+    }
 
-	public void setQnaAnswer(String qnaAnswer) {
-		this.qnaAnswer = qnaAnswer;
-	}
-	
-}	
+    public String getQnaDate() {
+        return qnaDate;
+    }
+
+    public void setQnaDate(String qnaDate) {
+        this.qnaDate = qnaDate;
+    }
+}
